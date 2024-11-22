bc38b2405d44a09ca8330f280ee6f03ec86616a8
iiasid <99832903+iiasid@users.noreply.github.com>
2024-11-22 14:26:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const brandSelect = document.getElementById('brand');
-  const modelSelect = document.getElementById('model');
+document.getElementById('brand').addEventListener('change', function() {
+  var brand = this.value;
+  var modelSelect = document.getElementById('model');
+  
+  // Réinitialiser les options du modèle
+  modelSelect.innerHTML = '<option value="">-- Choisir un modèle --</option>';
 
-  // Fonction qui gère l'activation du champ modèle selon la marque
-  brandSelect.addEventListener('change', function() {
-    const brand = brandSelect.value;
+  if (brand) {
+    // Activer la sélection du modèle
+    modelSelect.removeAttribute('disabled');
     
-    // Réinitialiser les modèles et désactiver la liste modèle
-    modelSelect.innerHTML = '<option value="">-- Choisir un modèle --</option>';
-    
-    if (brand) {
-      modelSelect.removeAttribute('disabled'); // Activer le modèle
-      let models = [];
+    // Définir les modèles disponibles selon la marque
+    let models = [];
 
-      switch (brand) {
-        case 'Toyota':
-          models = ['Yaris', 'Corolla', 'Hilux', 'Land Cruiser', 'Auris'];
-          break;
-        case 'BMW':
-          models = ['X2', '3 Series', 'X5', 'M3', 'Z4'];
-          break;
-        case 'Renault':
-          models = ['Twingo', 'Clio', 'Megane', 'Scenic', 'Kangoo'];
-          break;
-        case 'Mercedes':
-          models = ['A-Class', 'B-Class', 'C-Class', 'E-Class', 'S-Class'];
-          break;
-        case 'Peugeot':
-          models = ['208', '3008', '508', '5008', 'Partner'];
-          break;
-        case 'Volkswagen':
-          models = ['Golf', 'Polo', 'Passat', 'Tiguan', 'Jetta'];
-          break;
-        case 'Audi':
-          models = ['A1', 'A3', 'A4', 'Q5', 'Q7'];
-          break;
-        case 'Ford':
-          models = ['Fiesta', 'Focus', 'Mondeo', 'Kuga', 'EcoSport'];
-          break;
-        case 'Fiat':
-          models = ['500', 'Panda', 'Tipo', 'Doblo', 'Ulysse'];
-          break;
-        case 'Citroën':
-          models = ['C3', 'C4', 'C5 Aircross', 'Berlingo', 'DS3'];
-          break;
-        default:
-          models = [];
-      }
-
-      // Ajouter les modèles à la liste déroulante du modèle
-      models.forEach(function(model) {
-        const option = document.createElement('option');
-        option.value = model;
-        option.textContent = model;
-        modelSelect.appendChild(option);
-      });
-    } else {
-      modelSelect.setAttribute('disabled', true); // Désactiver le modèle si aucune marque sélectionnée
+    switch (brand) {
+      case 'Toyota':
+        models = ['Yaris', 'Corolla', 'Hilux', 'Land Cruiser', 'Auris'];
+        break;
+      case 'BMW':
+        models = ['X2', '3 Series', 'X5', 'M3', 'Z4'];
+        break;
+      case 'Renault':
+        models = ['Twingo', 'Clio', 'Megane', 'Scenic', 'Kangoo'];
+        break;
+      case 'Mercedes':
+        models = ['A-Class', 'B-Class', 'C-Class', 'E-Class', 'S-Class'];
+        break;
+      case 'Peugeot':
+        models = ['208', '3008', '508', '5008', 'Partner'];
+        break;
+      case 'Volkswagen':
+        models = ['Golf', 'Polo', 'Passat', 'Tiguan', 'Jetta'];
+        break;
+      case 'Audi':
+        models = ['A1', 'A3', 'A4', 'Q5', 'Q7'];
+        break;
+      case 'Ford':
+        models = ['Fiesta', 'Focus', 'Mondeo', 'Kuga', 'EcoSport'];
+        break;
+      case 'Fiat':
+        models = ['500', 'Panda', 'Tipo', 'Doblo', 'Ulysse'];
+        break;
+      case 'Citroën':
+        models = ['C3', 'C4', 'C5 Aircross', 'Berlingo', 'DS3'];
+        break;
+      default:
+        models = [];
     }
-  });
 
-  // S'assurer que le modèle est désactivé au départ
-  modelSelect.setAttribute('disabled', true);
+    // Ajouter les options de modèles à la liste
+    models.forEach(function(model) {
+      var option = document.createElement('option');
+      option.value = model;
+      option.textContent = model;
+      modelSelect.appendChild(option);
+    });
+  } else {
+    // Si aucune marque n'est sélectionnée, désactiver le modèle
+    modelSelect.setAttribute('disabled', true);
+  }
 });
